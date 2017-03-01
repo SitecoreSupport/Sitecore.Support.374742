d94061c09ce81a7436aa8eca2318ed6532c59aa6
Viktor Kucherov <vkucherov@live.ru>
2017-03-01 12:18:11 +0800

    no message

---
+++ src/Sitecore.Support.374742/Data/Comparers/CreatedComparer.cs
@@ -0,0 +1,40 @@
+﻿using Sitecore.Data.Comparers;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using System;
+
+namespace Sitecore.Support.Data.Comparers
+{
+  public class CreatedComparer : Sitecore.Data.Comparers.CreatedComparer
+  {
+    public override IKey ExtractKey(Item item)
+    {
+      Assert.ArgumentNotNull(item, "item");
+      //Spesifying of the Sortorder value fixes the issue
+      return new KeyObj
+      {
+        Item = item,
+        Key = this.GetCreationDate(item),
+        Sortorder = item.Appearance.Sortorder
+      };
+    }
+
+    private DateTime GetCreationDate(Item item)
+    {
+      DateTime maxValue = DateTime.MaxValue;
+      foreach (Item item2 in item.Versions.GetVersions(true))
+      {
+        DateTime created = item2.Statistics.Created;
+        if ((created != DateTime.MinValue) && (created.CompareTo(maxValue) < 0))
+        {
+          maxValue = created;
+        }
+      }
+      if (!(maxValue == DateTime.MaxValue))
+      {
+        return maxValue;
+      }
+      return DateTime.MinValue;
+    }
+  }
+}
---
+++ src/Sitecore.Support.374742/Data/Comparers/ReverseComparer.cs
@@ -0,0 +1,52 @@
+﻿using Sitecore.Data.Comparers;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+
+namespace Sitecore.Support.Data.Comparers
+{
+  public class ReverseComparer : ExtractedKeysComparer
+  {
+    public override IKey ExtractKey(Item item)
+    {
+      Assert.ArgumentNotNull(item, "item");
+      return new KeyObj
+      {
+        Item = item,
+        Key = item.Name,
+        Sortorder = item.Appearance.Sortorder
+      };
+    }
+
+    protected override int CompareKeys(IKey key1, IKey key2)
+    {
+      Assert.ArgumentNotNull(key1, "key1");
+      Assert.ArgumentNotNull(key2, "key2");
+      return CompareNames(key1.Key.ToString(), key2.Key.ToString());
+    }
+
+    protected override int DoCompare(Item item1, Item item2)
+    {
+      string name = item1.Name;
+      string str2 = item2.Name;
+      return CompareNames(name, str2);
+    }
+
+    private static int CompareNames(string name1, string name2)
+    {
+      Assert.ArgumentNotNull(name1, "name1");
+      Assert.ArgumentNotNull(name2, "name2");
+      if ((name1.Length > 0) && (name2.Length > 0))
+      {
+        if ((name1[0] == '_') && (name2[0] != '_'))
+        {
+          return -1;
+        }
+        if ((name2[0] == '_') && (name1[0] != '_'))
+        {
+          return 1;
+        }
+      }
+      return name2.CompareTo(name1);
+    }
+  }
+}
